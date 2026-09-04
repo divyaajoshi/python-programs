22f953d1616bddb9d6d52583c39d215125b5f276
Divya Joshi <cleartargetdj@gmail.com>
2026-09-04 22:44:17 +0530

    Add input output program.

---
+++ 02_input_output.py
@@ -0,0 +1,9 @@
+# Python program for Input and Output
+
+# Taking input from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Displaying output
+print("Hello", name)
+print("Your age is", age)
